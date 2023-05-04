bb05db9f7d19d9c152464677e3f5528e5c026b7e
Oz <ian.cosborne@yahoo.com>
2023-05-04 17:04:30 -0400

    Added issues and 404 page

---
+++ Shared/Issue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Issue
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string IssueUrl { get; set; }
+
+        public Issue(string name, string subtitle, string description,  string imagePath, string url)
+        {
+            Title = name;
+            Description = description;
+            Subtitle = subtitle;
+            ImagePath = imagePath;
+            IssueUrl = url;
+        }
+    }
+}
